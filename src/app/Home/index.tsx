07a331aa04f21fee9c7036a58a0634ebdd5350d0
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2026-01-15 19:44:58 -0300

    refactor: status icon and create component item

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
 
 import { Button }from '@/components/Button'
 import {Input } from '@/components/Input'
@@ -6,7 +6,8 @@ import { Filter } from '@/components/Filter';
 
 import { styles } from './styles';
 import { FilterStatus } from '@/types/FilterStatus';
-
+import { Item } from '@/components/item';
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING]
 export function Home() {
   return (
     <View style={styles.container}>
@@ -17,9 +18,19 @@ export function Home() {
         <Button title='Adicionar' onPress={()=> console.log("ok")}/>
       </View>
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive/>
-        <Filter status={FilterStatus.PENDING} isActive={false}/>
-
+        <View style={styles.header}>
+          { FILTER_STATUS.map((status)=> (
+          <Filter key={status} status={status} isActive/>
+          )
+          )}
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+        <Item 
+          data={{status: FilterStatus.DONE, description:"Arroz"}}
+          onRemove={()=> console.log("remove")}
+          onStatus={()=> console.log("muda")}/>
       </View>
 
     </View>
